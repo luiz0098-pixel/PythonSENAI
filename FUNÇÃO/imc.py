6aed91561932ea2825695fd8f194f7d10525c1cf
Luiz Fernando Mendes França <fernandofrancadossantos85@gmail.com>
2025-08-29 20:49:50 -0300

    Update imc.py

--- FUNÇÃO/imc.py
+++ FUNÇÃO/imc.py
@@ -1,22 +1,19 @@
-def conta_imc (peso, altura) :
+def conta_imc(peso, altura):
     imc_conta = peso / (altura * altura)
     return imc_conta
 
-
-def classificacao (imc) :
-    if imc <= 18.5:
-        print("abaixo do peso")
-    elif imc >= 18.5 <= 24.9:
-        print("peso normal")
-    elif imc >= 25:
-        print("sobrepeso")
+def classificacao(imc):
+    if imc < 18.5:
+        print("Abaixo do peso")
+    elif imc >= 18.5 and imc <= 24.9:
+        print("Peso normal")
+    elif imc >= 25 and imc <= 29.9:
+        print("Sobrepeso")
     elif imc >= 30:
-        print("obesidade")
-        
-        
-   
-peso = float(input("digite seu peso: "))
-altura = float(input("digite sua altura: "))
+        print("Obesidade")
+
+peso = float(input("Digite seu peso (kg): "))
+altura = float(input("Digite sua altura (m): "))
 imc = conta_imc(peso, altura)
-print("seu imc é", imc)
+print("Seu IMC é", round(imc, 2))
 classificacao(imc)
