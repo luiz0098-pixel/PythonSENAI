290ce92251fe0073f8556c92dcfea2e7c461cae1
Luiz Fernando Mendes França <fernandofrancadossantos85@gmail.com>
2025-08-29 20:47:32 -0300

    Update calculadora.py

--- FUNÇÃO/calculadora.py
+++ FUNÇÃO/calculadora.py
@@ -1,38 +1,44 @@
-def menu_calculadora():
-    print("escolha uma opção")
+def soma(n1, n2):
+    return n1 + n2
+
+def subtracao(n1, n2):   
+    return n1 - n2
+
+def multiplicacao(n1, n2):   
+    return n1 * n2
+
+def divisao(n1, n2):   
+    if n2 == 0:
+        return "Erro: divisão por zero!"
+    return n1 / n2
+
+def menu_calculadora(n1, n2):
+    print("Escolha uma opção:")
     print("1 - soma")
     print("2 - subtração")
-    print("3-  multiplicação")
-    print("4- divisão")
-    menu = input("coloque sua escolha aqui:")
+    print("3 - multiplicação")
+    print("4 - divisão")
+    
+    menu = input("Coloque sua escolha aqui: ")
     if menu == "1":
         print(soma(n1, n2))
     elif menu == "2":
-        print(subtração(n1, n2))
+        print(subtracao(n1, n2))
     elif menu == "3":
-        print(multiplicação(n1, n2))
+        print(multiplicacao(n1, n2))
     elif menu == "4":
-        print(divisão(n1, n2))
+        print(divisao(n1, n2))
     else:
-        print("você digitou o numero errado")
-    
-def soma (n1 , n2):
-        return n1 + n2
-def subtração(n1, n2):
-        return n1 - n2
-def multiplicação(n1, n2):
-        return n1 * n2
-def divisão (n1, n2):
-        return n1/n2
-    
-def resultado():
+        print("Você digitou um número errado.")
+
+def resultado(n1, n2):
     print(soma(n1, n2), "é o resultado da adição!")
-    print(subtração(n1, n2), "é o resultado da subtração!")
-    print(multiplicação(n1, n2), "é o resultado da multiplicação!")
-    print(divisão(n1, n2), "é o resultado da divisão!")
-    
-n1= float(input("digite um numero:"))
-n2 = float(input("digite outro numero:"))
+    print(subtracao(n1, n2), "é o resultado da subtração!")
+    print(multiplicacao(n1, n2), "é o resultado da multiplicação!")
+    print(divisao(n1, n2), "é o resultado da divisão!")
 
-menu_calculadora()
+# programa principal
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
 
+menu_calculadora(n1, n2)
